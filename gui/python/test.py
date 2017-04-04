aef18cd05edfca43f9dc892ddabba8eee8db51b2
Tobias Hiort <hiort.tobias@gmail.com>
2017-04-04 19:55:43 +0200

    Fix for non-trailing movement and unrestricted layer actions

--- gui/python/test.py
+++ gui/python/test.py
@@ -4,12 +4,11 @@ import os
 import numpy
 import math
 import time
-
 import tkinter as tk # replace
-from tkinter import filedialog # remove?
 
+from tkinter import filedialog # remove?
 from PIL import Image
-from pygame import gfxdraw
+#from pygame import gfxdraw
 from pygame.locals import *
 
 # constants
@@ -21,13 +20,14 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_GREEN = (0, 255, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_BACKGROUND = (245, 245, 245)
+COLOR_KEY = (1, 1, 1)
 
 PADDING_MAP = 10
 
-PLAYER_SCALE = 2.0 # automate this depending on image res. min 1, max 2?
+PLAYER_SCALE = 0.5 # automate this depending on image res. min 1, max 2?
 
 # read image to matrix
-mapImage = Image.open(os.path.join('maps', 'map6.png'))
+mapImage = Image.open(os.path.join('maps', 'map3.png'))
 mapRGBA = mapImage.load()
 mapMatrix = numpy.zeros((mapImage.size[1], mapImage.size[0])) # (rows, column)
 
@@ -79,16 +79,22 @@ displaySurface = pygame.display.set_mode((GAME_RES)) # ,pygame.NOFRAME
 displaySurface.fill(COLOR_BACKGROUND)
 pygame.display.set_caption(GAME_NAME)
 
-# map/simulation surface
+# map surface
 mapSurface = pygame.Surface((907-0*PADDING_MAP, 713-PADDING_MAP))
 mapSurface = mapSurface.convert()
 mapSurface.fill(COLOR_BACKGROUND)
 
+# player surface
+playerSurface = pygame.Surface((907-0*PADDING_MAP, 713-PADDING_MAP))
+playerSurface = playerSurface.convert()
+playerSurface.set_colorkey(COLOR_KEY)
+
 # statistics surface
-statisticsSurface = pygame.Surface((1024, 713)) # x=1024?
+statisticsSurface = pygame.Surface((1024, 713)) # x=1024? 
 statisticsSurface = statisticsSurface.convert()
 statisticsSurface.fill(COLOR_RED)
 
+# settings surface
 settingsSurface = pygame.Surface((1024, 713)) # x=1024?
 settingsSurface = statisticsSurface.convert()
 settingsSurface.fill(COLOR_GREEN)
@@ -128,11 +134,12 @@ options['title'] = 'Select Map'
 # create the map with draw.rect on mapSurface
 for row in range(MAPHEIGHT):
     for column in range(MAPWIDTH):
-        pygame.draw.rect(mapSurface, colors[mapMatrix[row][column]], (column*TILESIZE+((907-2*PADDING_MAP)/(2))-((MAPWIDTH*TILESIZE)/2)+PADDING_MAP, (row*TILESIZE+((713-1*PADDING_MAP)/(2))-((MAPHEIGHT*TILESIZE)/2)), TILESIZE, TILESIZE)) 
+        pygame.draw.rect(mapSurface, colors[mapMatrix[row][column]], (math.floor(column*TILESIZE+((907-2*PADDING_MAP)/(2))-((MAPWIDTH*TILESIZE)/2)+PADDING_MAP), (row*TILESIZE+((713-1*PADDING_MAP)/(2))-((MAPHEIGHT*TILESIZE)/2)), TILESIZE, TILESIZE)) 
 
 # game loop
 while True:
     for event in pygame.event.get():
+        # add 'if' for active tab so you cant click on another layer... or move player etc.
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -140,32 +147,33 @@ while True:
         elif event.type == KEYDOWN:
             # change player pos which will be rendered in the next frame. Remove later, but save mechanics.
             # [0] = column, [1] = row
-            if event.key == K_d:
-                playerPos[0] += 1
-            if event.key == K_a:
-                playerPos[0] -= 1
-            if event.key == K_x:
-                playerPos[1] += 1
-            if event.key == K_w:
-                playerPos[1] -= 1
-            if event.key == K_q:
-                playerPos[0] -= 1
-                playerPos[1] -= 1
-            if event.key == K_e:
-                playerPos[0] += 1
-                playerPos[1] -= 1
-            if event.key == K_z:
-                playerPos[0] -= 1
-                playerPos[1] += 1
-            if event.key == K_c:
-                playerPos[0] += 1
-                playerPos[1] += 1
-            elif event.key == K_u:
-                root = tk.Tk()
-                root.withdraw()
-                file_path = filedialog.askopenfilename(**file_opt)
-                filename_pos = file_path.rfind('/')+1 # position for filename
-                print(file_path[filename_pos:]) # expand from here, probably need to create funcions for rendering before?
+            if active_tab[0]:
+                if event.key == K_d:
+                    playerPos[0] += 1
+                if event.key == K_a:
+                    playerPos[0] -= 1
+                if event.key == K_x:
+                    playerPos[1] += 1
+                if event.key == K_w:
+                    playerPos[1] -= 1
+                if event.key == K_q:
+                    playerPos[0] -= 1
+                    playerPos[1] -= 1
+                if event.key == K_e:
+                    playerPos[0] += 1
+                    playerPos[1] -= 1
+                if event.key == K_z:
+                    playerPos[0] -= 1
+                    playerPos[1] += 1
+                if event.key == K_c:
+                    playerPos[0] += 1
+                    playerPos[1] += 1
+                elif event.key == K_u:
+                    root = tk.Tk()
+                    root.withdraw()
+                    file_path = filedialog.askopenfilename(**file_opt)
+                    filename_pos = file_path.rfind('/')+1 # position for filename
+                    print(file_path[filename_pos:]) # expand from here, probably need to create funcions for rendering before?
         # mouse motion events
         elif event.type == MOUSEMOTION:
             mouse_x, mouse_y = event.pos
@@ -174,21 +182,21 @@ while True:
             elif active_tab[0] == True:
                 displaySurface.blit(BUTTON_SIMULATION_ACTIVE, (0,0))
             else:
-            	displaySurface.blit(BUTTON_SIMULATION_BLANK, (0, 0))
+                displaySurface.blit(BUTTON_SIMULATION_BLANK, (0, 0))
             
             if (mouse_x > 202) and (mouse_x <= 382) and (mouse_y >= 0) and (mouse_y <= 45) and not(active_tab[1]):
                 displaySurface.blit(BUTTON_SETTINGS_HOVER, (202,0))
             elif active_tab[1] == True:
                 displaySurface.blit(BUTTON_SETTINGS_ACTIVE, (202,0))
             else:
-            	displaySurface.blit(BUTTON_SETTINGS_BLANK, (202, 0))
+                displaySurface.blit(BUTTON_SETTINGS_BLANK, (202, 0))
             
             if (mouse_x > 382) and (mouse_x <= 575) and (mouse_y >= 0) and (mouse_y <= 45) and not(active_tab[2]):
                 displaySurface.blit(BUTTON_STATISTICS_HOVER, (382,0))
             elif active_tab[2] == True:
                 displaySurface.blit(BUTTON_STATISTICS_ACTIVE, (382,0))
             else:
-            	displaySurface.blit(BUTTON_STATISTICS_BLANK, (382, 0))
+                displaySurface.blit(BUTTON_STATISTICS_BLANK, (382, 0))
         # mouse button events
         elif event.type == MOUSEBUTTONDOWN:
             # left click
@@ -213,9 +221,6 @@ while True:
                     displaySurface.blit(BUTTON_STATISTICS_ACTIVE, (382, 0))
                     active_tab = [False, False, True]
 
-    # draw player on simulation tab/mapsurface
-    pygame.draw.circle(mapSurface, COLOR_GREEN, ((playerPos[0]*TILESIZE + math.floor(TILESIZE/2) + math.floor(((907-2*PADDING_MAP)/(2))-((MAPWIDTH*TILESIZE)/2)+PADDING_MAP)), playerPos[1]*TILESIZE+round(TILESIZE/2) + round(0*TILESIZE+((713-1*PADDING_MAP)/(2))-((MAPHEIGHT*TILESIZE)/2))), round((TILESIZE/2)*PLAYER_SCALE))
-
     # settings page
     settings_text = FONT_ROBOTOMEDIUM24.render("settingsSurface placeholder", 0, COLOR_BLACK)
     settingsSurface.blit(settings_text, (200, 300))
@@ -225,16 +230,22 @@ while True:
     statisticsSurface.blit(statistics_text, (200, 300))
 
     if active_tab[0]:
+        # remove last frame
+        playerSurface.fill(COLOR_KEY)
+        # draw player on simulation tab/mapsurface
+        pygame.draw.circle(playerSurface, COLOR_GREEN, ((playerPos[0]*TILESIZE + math.floor(TILESIZE/2) + math.floor(((907-2*PADDING_MAP)/(2))-((MAPWIDTH*TILESIZE)/2)+PADDING_MAP)), playerPos[1]*TILESIZE+round(TILESIZE/2) + round(0*TILESIZE+((713-1*PADDING_MAP)/(2))-((MAPHEIGHT*TILESIZE)/2))), round((TILESIZE/2)*PLAYER_SCALE))
+        # important order
         displaySurface.blit(mapSurface, (0*PADDING_MAP, 55))
+        displaySurface.blit(playerSurface, (0*PADDING_MAP, 55))
         displaySurface.blit(MENU_RIGHT, (907, 45))
     elif active_tab[1]:
-    	displaySurface.blit(settingsSurface, (0*PADDING_MAP, 55))
-    	displaySurface.blit(MENU_FADE, (0, 45))
+        displaySurface.blit(settingsSurface, (0*PADDING_MAP, 55))
+        displaySurface.blit(MENU_FADE, (0, 45))
     elif active_tab[2]:
-    	displaySurface.blit(statisticsSurface, (0*PADDING_MAP, 55))
-    	displaySurface.blit(MENU_FADE, (0, 45))
+        displaySurface.blit(statisticsSurface, (0*PADDING_MAP, 55))
+        displaySurface.blit(MENU_FADE, (0, 45))
     else:
-    	print('Error: No active tab')
+        print('Error: No active tab')
 
     # just the messy width and height from above... remove later
     #print((0*TILESIZE+((907-2*PADDING_MAP)/(2))-((MAPWIDTH*TILESIZE)/2)+PADDING_MAP)) # width
