bcef3d1abfa2805585aff6e658e228840deddef8
Linn <lilo4591@student.uu.se>
2017-05-18 15:32:17 +0200

    doorstats is now working

--- src/stats.go
+++ src/stats.go
@@ -27,15 +27,15 @@ func readStats(peopleArray []*Person, inmap [][]tile) {
 	fmt.Fprintf(pplfile, s)
 
 
-	mapfile, err2 := os.Create("mapStats.txt") //how many tiles are on fire?, most used exit
-	if err2 != nil {
+	mapfile, err2 := os.Create("mapStats.txt")
+ //[how many tiles are on fire?], [amount exited per door], [exit-doorcordinates]
+  if err2 != nil {
 		log.Fatal("Cannot create file, map")
 	}
 	defer mapfile.Close()
 
 	//burning tiles
 	burningTiles := MapStats(inmap)
-	//fmt.Print(mapStats)
 
 	bytes2, err2 = json.Marshal(burningTiles)
 	if err2 != nil {
@@ -44,64 +44,73 @@ func readStats(peopleArray []*Person, inmap [][]tile) {
 	str := string(bytes2[:])
 	fmt.Fprintf(mapfile, str)
 
-	/*
-	most used exitsdoors
-	mostUsedDoors := doorStats(peopleArray, inmap)
 
-	fmt.Print(mostUsedDoors)
-	bytes2, err2 = json.Marshal(mostUsedDoors)
+	//most used exitsdoors
+	noUsed, doorCoord := doorStats(peopleArray, inmap)
+
+	bytes2, err2 = json.Marshal(noUsed)
 	if err2 != nil {
 		panic(err2)
 	}
-	*/
+
 	str2 := string(bytes2[:])
-	fmt.Fprintf(mapfile, str2)
+	fmt.Println(str2)
+  fmt.Fprintf(mapfile, str2)
 	fmt.Print(str2)
 
+	bytes2, err2 = json.Marshal(doorCoord)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	str3 := string(bytes2[:])
+	fmt.Println(str3)
+  fmt.Fprintf(mapfile, str3)
+	fmt.Print(str3)
 	//mapfile.Close()
 }
-/*
+
 //TODO most used exit door
 //TODO dis function doesnt wörk, no exit is found
-func doorStats(peopleArray []*Person, inmap [][]tile) []int {
+func doorStats(peopleArray []*Person, inmap [][]tile) ([]int, [][]int) {
 
 	doors := DoorCoord(inmap)
-	//debug
-	fmt.Print(doors)
-	var statistics []int
-	index := (len(peopleArray) - 2) //vi behöver näst sista kordinaten
+  numberOfExits := make([]int, len(doors))
 
 	var tmpx int
 	var tmpy int
 	for  i := 0; i < (len(peopleArray)); i++ {
-		tmpx = peopleArray[i].path[index].xCoord
-		tmpy = peopleArray[i].path[index].yCoord
-		for j := 0; j < (len(doors)); j++ {
-			if tmpx == doors[j][0] && tmpy == doors[j][1] {
-				doorStats[j] ++
-			}
-		}
-	}
-	return statistics
-}
-*/
 
+    index := (len(peopleArray[i].path) - 2) //vi behöver näst sista kordinaten
+    tmpx = peopleArray[i].path[index].xCoord
+		tmpy = peopleArray[i].path[index].yCoord
+    for j := 0; j < (len(doors)); j++ {
+      if tmpx == doors[j][0] && tmpy == doors[j][1] {
+
+        numberOfExits[j] = numberOfExits[j] + 1
+      }
+    }
+  }
+	return numberOfExits, doors
+}
 
 //TODO average escape time
 //TODO write average escapetime to file
 //TODO call this func at end of simuation
 func averageExitTime(peopleArray []*Person) float32 {
 
-	var totalTime float32
+  var totalTime float32
 
-	size := len(peopleArray)
-	for _, p := range peopleArray {
-		totalTime = totalTime + p.time
-	}
-	if size != 0 {
-		// fmt.Print(totalTime/float32(size))
-		return (totalTime/float32(size))
-	}else {return 0}
+  size := len(peopleArray)
+  for i, p := range peopleArray {
+    if (peopleArray[i].alive == true) {
+      totalTime = totalTime + p.time
+    }
+  }
+  if size != 0 {
+    // fmt.Print(totalTime/float32(size))
+    return (totalTime/float32(size))
+  }else {return 0}
 }
 
 
@@ -126,7 +135,6 @@ func averageExitHealth(peopleArray []*Person) int {
 //TODO took most damage from smoke/fire
 //relevant? on individual lvl? 
 
-/*
 
 func main() {
 
@@ -134,14 +142,15 @@ func main() {
 		{1, 1, 3, 1, 1, 1, 1},
 		{1, 0, 0, 0, 0, 0, 0},
 		{1, 0, 0, 1, 0, 0, 0},
-		{1, 0, 0, 1, 0, 0, 0},
+		{1, 0, 0, 1, 0, 0, 2},
 		{1, 0, 0, 1, 0, 0, 0},
 		{3, 0, 0, 0, 0, 0, 0},
-		{1, 0, 0, 0, 0, 0, 2}}
+		{1, 0, 2, 0, 0, 0, 2}}
 
 		ppl := [][]int{
-			{0, 0},
+			{1, 1},
 			{0, 6},
+      {0, 5},
 			{2, 4}}
 
 			testmap := TileConvert(matrix)
@@ -157,10 +166,5 @@ func main() {
 			for i, p := range pplArray {
 				fmt.Println("Person", i, "time:  ", p.time, "\n         health:", p.hp)
 			}
-
-			doorstat := doorStats(pplArray, testmap)
-			for i := 0; i < len(doorstat); i++ {
-				fmt.Print(doorstat[i])
-			}
 }
-*/
+
